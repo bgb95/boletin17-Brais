264552191a4c35d545c2c0557a3b6fac109af17c
bgb95 <braisgb@gmail.com>
2017-05-04 10:32:09 +0200

    Exercicios

--- Boletin17-Junit/src/Exercicios/Xeometria.java
+++ Boletin17-Junit/src/Exercicios/Xeometria.java
@@ -1,5 +1,24 @@
 package Exercicios;
 
 public class Xeometria {
-	
+
+	public void perímetroCadrado(int lado) {
+		int perímetroCadrado;
+
+		lado = 4;
+		perímetroCadrado = lado * 4;
+		System.out.print(perímetroCadrado);
+	}
+
+	public void areaCadrado(int lado) {
+		int areaCadrado;
+
+		lado = 4;
+		areaCadrado = lado * 2;
+		System.out.print(areaCadrado);
+	}
+
+	public void teoremaPitagoras(int catetoA, int catetoB) {
+		
+	}
 }
