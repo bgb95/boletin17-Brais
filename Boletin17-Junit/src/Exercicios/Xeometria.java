b55bce3007402d4f43639e9503b4347c084a2b72
bgb95 <braisgb@gmail.com>
2017-05-09 11:45:22 +0200

    Exercicio 5, apartados a, b, c

--- Boletin17-Junit/src/Exercicios/Xeometria.java
+++ Boletin17-Junit/src/Exercicios/Xeometria.java
@@ -8,4 +8,22 @@ public class Xeometria {
 		else
 			return lado * 4;
 	}
+
+	public int areaCadrado(int lado) {
+		if (lado < 0)
+			return 0;
+		else
+			return lado * lado;
+	}
+
+	public double teoremaPitagoras(int catetoA, int catetoB) {
+		double hipotenusa;
+
+		if (catetoA < 0 || catetoB < 0)
+			return 0;
+		else {
+			hipotenusa = Math.sqrt(catetoA * catetoA + catetoB * catetoB);
+			return hipotenusa;
+		}
+	}
 }
---
+++ Boletin17-Junit/src/Exercicios/XeometriaTest.java
@@ -0,0 +1,55 @@
+package Exercicios;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class XeometriaTest {
+	Xeometria prueba = new Xeometria();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testperímetroCadrado() {
+		assertEquals(prueba.perímetroCadrado(1), 4);
+		assertEquals(prueba.perímetroCadrado(2), 8);
+		assertEquals(prueba.perímetroCadrado(3), 12);
+		assertEquals(prueba.perímetroCadrado(-1), 0);
+		assertEquals(prueba.perímetroCadrado(-20), 0);
+	}
+
+	@Test
+	public void testareaCadrado() {
+		assertEquals(prueba.areaCadrado(1), 1);
+		assertEquals(prueba.areaCadrado(2), 4);
+		assertEquals(prueba.areaCadrado(3), 9);
+		assertEquals(prueba.areaCadrado(-20), 0);
+		assertEquals(prueba.areaCadrado(-1), 0);
+	}
+	
+	@Test
+	public void testteoremaPitagoras() {
+		assertEquals(prueba.teoremaPitagoras(-1,1), 0,0.001);
+		assertEquals(prueba.teoremaPitagoras(1,-1), 0, 0.001);
+		assertEquals(prueba.teoremaPitagoras(1,1), Math.sqrt(2), 0.001);
+		assertEquals(prueba.teoremaPitagoras(2,5), Math.sqrt(29), 0.001);
+	}
+}
