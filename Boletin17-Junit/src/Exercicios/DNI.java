57ecf225e62ccbe9e314e70d82be1c4e1db35e38
bgb95 <braisgb@gmail.com>
2017-05-10 11:34:51 +0200

    Exercicio 6 Apartado a,b

--- Boletin17-Junit/src/Exercicios/DNI.java
+++ Boletin17-Junit/src/Exercicios/DNI.java
@@ -1,8 +1,8 @@
 package Exercicios;
 
 public class DNI {
-	private String[] letraDNI = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q",
-			"V", "H", "L", "C", "K", "E" };
+	//private String[] letraDNI = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q",
+			//"V", "H", "L", "C", "K", "E" };
 
 	public boolean eValido(String numeroLetra) {
 		if (numeroLetra.length() == 9) {
