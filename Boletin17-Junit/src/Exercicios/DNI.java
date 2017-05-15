f0d2d5a471ffd3cd17b27d5329777c456dcbad7b
bgb95 <braisgb@gmail.com>
2017-05-15 13:46:25 +0200

    Exercicio 6

--- Boletin17-Junit/src/Exercicios/DNI.java
+++ Boletin17-Junit/src/Exercicios/DNI.java
@@ -1,27 +1,59 @@
 package Exercicios;
 
 public class DNI {
-	//private String[] letraDNI = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q",
-			//"V", "H", "L", "C", "K", "E" };
 
-	public boolean eValido(String numeroLetra) {
-		if (numeroLetra.length() == 9) {
-			char[] arrayChar = numeroLetra.toCharArray();
-			for (int i = 0; i <= arrayChar.length; i++) {
-				if (i < 8)
-					if (arrayChar[i] < '1' && arrayChar[i] > '9')
-						return false;
-				if (i == 8)
-					if (arrayChar[i] < 'A' && arrayChar[i] < 'Z')
-						return false;
-			}
+	public boolean eValido() {
+		String numeroLetra="";
+		String soloNumeros="";
+		
+		if (DNI.length() != 9 || Character.isLetter(this.DNI.chartAt(8))==false) {
+			return false;
+		}
+		
+		numeroLetra=(this.DNI.substring(8)).toUpperCase();
+		soloNumeros=(this.DNI.substring(8)).toUpperCase();
+		
+		if(soloNumeros()==true && letraDNI.equals(numeroLetra)) {
 			return true;
-		} else
+		}
+		else
 			return false;
 	}
 	
-	/*public int calculaLetra(String numero) {
-		int resultado;
+	private String letraDNI() {
 		
-	}*/
+		int miDNI=Integer.parseInt(this.DNI.substring(0,8));
+		int resto=0;
+		String miLetra="";
+		String[] asignacionLetra={"T", "R", "W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+		
+		resto=miDNI%23;
+		
+		miLetra=asignacionLetra[resto];
+		
+		return miLetra;
+	}
+	
+	private boolean soloNumeros() {
+		
+		int i,j=0;
+		String numero="";
+		String miDNI="";
+		String[] unoNueve={"0","1","2","3","4","5","6","7","8","9"};
+		
+		for(i=0; i<this.letraDNI().length() - 1; i++) {
+			numero =this.letraDNI().substring(i, i+1);
+			
+			for(j=0; j<unoNueve.length; j++) {
+				if(numero.equals(unoNueve[j])) {
+					miDNI=unoNueve[j];
+				}
+			}
+		}
+		
+		if(miDNI.length()!=8)
+			return false;
+		else
+			return true;
+	}
 }
