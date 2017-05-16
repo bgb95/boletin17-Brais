735654f61a68287c493d1b5971bca25074b41a3d
bgb95 <braisgb@gmail.com>
2017-05-16 11:46:33 +0200

    Exercicio 6 sin casos de uso

--- Boletin17-Junit/src/Exercicios/DNI.java
+++ Boletin17-Junit/src/Exercicios/DNI.java
@@ -2,56 +2,55 @@ package Exercicios;
 
 public class DNI {
 
+	private String DNI;
+
 	public boolean eValido() {
-		String numeroLetra="";
-		String soloNumeros="";
-		
-		if (DNI.length() != 9 || Character.isLetter(this.DNI.chartAt(8))==false) {
+		String numeroLetra = "";
+
+		if (DNI.length() != 9 || Character.isLetter(this.DNI.charAt(8)) == false)
 			return false;
-		}
-		
-		numeroLetra=(this.DNI.substring(8)).toUpperCase();
-		soloNumeros=(this.DNI.substring(8)).toUpperCase();
-		
-		if(soloNumeros()==true && letraDNI.equals(numeroLetra)) {
+
+		numeroLetra = (this.DNI.substring(8)).toUpperCase();
+
+		if (soloNumeros() == true && letraDNI().equals(numeroLetra))
 			return true;
-		}
 		else
 			return false;
 	}
-	
-	private String letraDNI() {
-		
-		int miDNI=Integer.parseInt(this.DNI.substring(0,8));
-		int resto=0;
-		String miLetra="";
-		String[] asignacionLetra={"T", "R", "W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
-		
-		resto=miDNI%23;
-		
-		miLetra=asignacionLetra[resto];
-		
+
+	public String letraDNI() {
+
+		int miDNI = Integer.parseInt(this.DNI.substring(0, 8));
+		int resto = 0;
+		String miLetra = "";
+		String[] asignacionLetra = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S",
+				"Q", "V", "H", "L", "C", "K", "E" };
+
+		resto = miDNI % 23;
+
+		miLetra = asignacionLetra[resto];
+
 		return miLetra;
 	}
-	
-	private boolean soloNumeros() {
-		
-		int i,j=0;
-		String numero="";
-		String miDNI="";
-		String[] unoNueve={"0","1","2","3","4","5","6","7","8","9"};
-		
-		for(i=0; i<this.letraDNI().length() - 1; i++) {
-			numero =this.letraDNI().substring(i, i+1);
-			
-			for(j=0; j<unoNueve.length; j++) {
-				if(numero.equals(unoNueve[j])) {
-					miDNI=unoNueve[j];
+
+	public boolean soloNumeros() {
+
+		int i, j = 0;
+		String numero = "";
+		String miDNI = "";
+		String[] unoNueve = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+		for (i = 0; i < this.letraDNI().length() - 1; i++) {
+			numero = this.letraDNI().substring(i, i + 1);
+
+			for (j = 0; j < unoNueve.length; j++) {
+				if (numero.equals(unoNueve[j])) {
+					miDNI = unoNueve[j];
 				}
 			}
 		}
-		
-		if(miDNI.length()!=8)
+
+		if (miDNI.length() != 8)
 			return false;
 		else
 			return true;
